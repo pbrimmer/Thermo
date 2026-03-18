f250b5e63ae33200264081d193a106aee357468a
pbrimmer <pbr27@cam.ac.uk>
2026-03-18 11:34:49 +0000

    Got rid of the plot (students can make that) and updated/corrected labels and plotted total pressure.

--- Demo5/extent.py
+++ Demo5/extent.py
@@ -10,6 +10,7 @@ K = (2.0*xi)**2.0/((1.0-xi)*(10.0 - 3.0*xi)**3.0)
 
 plt.xscale('log')
 plt.yscale('log')
+plt.plot(K,2.0*xi + 1.0-xi + 10.0-3.0*xi,'k-',lw=3,label='total pressure')
 plt.plot(K,1.0-xi,'b-',lw=2,label='N$_2$')
 plt.plot(K,10.0-3.0*xi,'r--',lw=2,label='H$_2$')
 plt.plot(K,2.0*xi,'g-.',lw=2,label='NH$_3$')
@@ -21,7 +22,7 @@ plt.tick_params(axis='both', which='minor',
                 length=3, width=1)
 
 plt.xlabel(r'$K_{\rm eq}$',fontsize=14)
-plt.ylabel(r'concentration',fontsize=14)
+plt.ylabel(r'pressure (bar)',fontsize=14)
 
 plt.legend(loc='upper left', bbox_to_anchor=(1.02, 1), borderaxespad=0.)
 plt.savefig('./Equilibrium.pdf',bbox_inches='tight')
